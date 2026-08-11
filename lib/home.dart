779e5ff03a0c82a80002ca314372de1aca8afaba
인성 <twapet0124@gmail.com>
2026-08-11 17:07:12 +0900

    로그인 화면

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -8,8 +9,91 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late TextEditingController userIdEditingController;
+  late TextEditingController passwordEditingController;
+
+  @override
+  void initState() {
+    super.initState();
+    userIdEditingController = TextEditingController();
+    passwordEditingController = TextEditingController();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Login Page'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            SizedBox(
+              height: 300,
+              child: Image.asset(
+                'images/monsterball.jpg',
+                fit: BoxFit.cover,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: userIdEditingController,
+                decoration: InputDecoration(
+                  labelText: 'ID를 입력하시오'
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: passwordEditingController,
+                decoration: InputDecoration(
+                  labelText: '비밀번호를 입력하시오'
+                ),
+                obscureText: true
+              ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                if(userIdEditingController.text.trim().isEmpty || passwordEditingController.text.trim().isEmpty) {
+                  errorSnackBar();
+                } else {
+                  _showSnackBar();
+                }
+              },
+              child: Text('Login')
+            ),
+            SizedBox(
+              height: 100,
+            )
+          ],
+        ),
+      ),
+    );
   }
+
+  // ----- Function -----
+  void errorSnackBar() {
+    Get.snackbar(
+      '경고',
+      'ID와 비밀번호를 입력하세요',
+      backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+    );
+  }
+
+  void _showSnackBar() {
+    Get.defaultDialog(
+      title: '로그인 완료',
+      middleText: '환영합니다 ${userIdEditingController.text.trim()}님!',
+      barrierDismissible: true, // false
+      backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+      actions: [
+        // TextButton(onPressed: Get.to(다음경로), child: Text('Exit'))
+      ]
+    );
+  }
+
 }
